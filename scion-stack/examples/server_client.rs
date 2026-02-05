4752fd928e0ae3fe529f9b3254c8604d2b236217
Tobias Buner <buner@anapaya.net>
2026-02-05 18:10:05 +0100

    chore(deps): remove scionstack dependency from pocketscion
    GitOrigin-RevId: 709e5de767bd56598e23a0f67843019f1a37afd0

--- scion-stack/examples/server_client.rs
+++ scion-stack/examples/server_client.rs
@@ -11,7 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-//! Simple end-to-end example using PocketScion and a ScionStack
+//! Simple end-to-end example using a ScionStack with Pocket SCION.
 //!
 //! ## Overview
 //!
